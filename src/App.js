8c1d81d88239ba35ef95937ead38d59879ccc772
Alef Freitas <alef.freitas07@gmail.com>
2022-01-20 15:35:58 -0300

    Iniciado Modal

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ import { Serve } from "./components/Serve";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
+
+  
+
   return (
     <>
+      
       <Header />
       <Main />
       <Recipes />
--- src/components/Header/Nav/RightNav/RightNav.js
+++ src/components/Header/Nav/RightNav/RightNav.js
@@ -1,17 +1,37 @@
 import { Ul } from "./styles";
-import {Link} from 'react-scroll';
+import { Link } from 'react-scroll';
+import Modal from 'react-modal';
+import { useState } from "react";
 
 
-export function RightNav({open,setOpen}) {
+export function RightNav({ open, setOpen}) {
+
+    const [isNewModalOpen, setisNewModalOpen] = useState(false);
+
+    function handleModalOpen() {
+        setisNewModalOpen(true);
+    }
+
+    function handleCloseModalOpen() {
+        setisNewModalOpen(false);
+    }
+
     return (
         <Ul open={open}>
             <Link open={open} onClick={() => setOpen(!open)} to="healthyRecipes" spy={true} smooth={true}> <li>HEALTHY RECIPES</li> </Link>
             <Link open={open} onClick={() => setOpen(!open)} to="blog" spy={true} smooth={true}><li>BLOG</li> </Link>
             <Link open={open} onClick={() => setOpen(!open)} to="join" spy={true} smooth={true}><li>JOIN</li></Link>
             <li>
-                <button type="button" open={open} onClick={() => setOpen(!open)}>
+                <button type="button" onClick={handleModalOpen}>
                     REGISTER
                 </button>
+
+                <Modal
+                    isOpen={isNewModalOpen}
+                    onRequestClose={handleCloseModalOpen}
+                >
+                    <h2>teste</h2>
+                </Modal>
             </li>
         </Ul>
     );
